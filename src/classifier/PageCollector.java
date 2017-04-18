93702158962109f7d0d2e9f19364114f7fcfc486
Pedro Henrique <pedro00dk@live.com>
2017-04-17 21:43:12 -0300

    Generate files with the stemmed page content

--- src/classifier/PageCollector.java
+++ src/classifier/PageCollector.java
@@ -2,39 +2,57 @@ package classifier;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import weka.core.stemmers.LovinsStemmer;
+import weka.core.stemmers.Stemmer;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Download the pages to be used in the classifier.
+ *
+ * @author Pedro Henrique
  */
 public class PageCollector {
 
     /**
      * Path to the negative page links.
      */
-    static Path negativePageLinksPath = Paths.get("pages", "negative.txt");
+    public static final Path negativePageLinksPath = Paths.get("pages", "negative.txt");
 
     /**
      * Path to the positive page links.
      */
-    static Path positivePageLinksPath = Paths.get("pages", "positive.txt");
+    public static final Path positivePageLinksPath = Paths.get("pages", "positive.txt");
 
     /**
      * Path to save the downloaded negative pages.
      */
-    static Path negativeDownloadedPagesPath = Paths.get("pages", "downloaded", "negative");
+    public static final Path negativeDownloadedPagesPath = Paths.get("pages", "downloaded", "negative");
 
     /**
      * Path to save the downloaded positive pages.
      */
-    static Path positiveDownloadedPagesPath = Paths.get("pages", "downloaded", "positive");
+    public static final Path positiveDownloadedPagesPath = Paths.get("pages", "downloaded", "positive");
+
+    /**
+     * Path to save the stemmed negative pages.
+     */
+    public static final Path negativeStemmedPagesPath = Paths.get("pages", "stemmed", "negative");
+
+    /**
+     * Path to save the stemmed positive pages.
+     */
+    public static final Path positiveStemmedPagesPath = Paths.get("pages", "stemmed", "positive");
 
     /**
      * Download the positive and negative pages.
@@ -50,6 +68,7 @@ public class PageCollector {
                         .filter(link -> !link.startsWith("#") && link.trim().length() != 0)
                         .collect(Collectors.toList()),
                 negativeDownloadedPagesPath,
+                negativeStemmedPagesPath,
                 "Page "
         );
 
@@ -59,6 +78,7 @@ public class PageCollector {
                         .filter(link -> !link.startsWith("#") && link.trim().length() != 0)
                         .collect(Collectors.toList()),
                 positiveDownloadedPagesPath,
+                positiveStemmedPagesPath,
                 "Page "
         );
     }
@@ -66,11 +86,12 @@ public class PageCollector {
     /**
      * Download the received pages and saves the html in the received directory.
      *
-     * @param links     the links to download
-     * @param directory the destination directory
-     * @param prefix    the file name prefix
+     * @param links             the links to download
+     * @param downloadDirectory the destination directory
+     * @param stemDirectory     the directory of the stemmed content
+     * @param prefix            the file name prefix
      */
-    static void downloadPages(List<String> links, Path directory, String prefix) {
+    private static void downloadPages(List<String> links, Path downloadDirectory, Path stemDirectory, String prefix) {
         AtomicInteger downloadedPages = new AtomicInteger(0);
         links.stream().parallel()
                 .forEach(link -> {
@@ -78,8 +99,15 @@ public class PageCollector {
                             System.out.println("Downloading link (" + currentPage + "): " + link);
                             try {
                                 Document document = Jsoup.connect(link).get();
-                                Files.newBufferedWriter(Paths.get(directory.toString(), prefix + currentPage + ".html"))
+                                Files.newBufferedWriter(Paths.get(downloadDirectory.toString(), prefix + currentPage + ".html"))
                                         .append(document.html())
+                                        .close();
+                                System.out.println("Stemming link (" + currentPage + "): " + link);
+                                Files.newBufferedWriter(Paths.get(stemDirectory.toString(), prefix + currentPage + ".stem.txt"))
+                                        .append(stemAndCount(document.text().split(" ")).entrySet().stream()
+                                                .sorted(Comparator.comparingLong(Map.Entry::getValue))
+                                                .map(entry -> entry.getKey() + " " + entry.getValue())
+                                                .collect(Collectors.joining("\n")))
                                         .close();
                                 System.out.println("Link (" + currentPage + ") Success");
                             } catch (IOException ignored) {
@@ -87,5 +115,19 @@ public class PageCollector {
                             }
                         }
                 );
+    }
+
+
+    /**
+     * Stem the received words and count then.
+     *
+     * @param words the words to stem and count
+     * @return a map of words the their frequency
+     */
+    public static Map<String, Long> stemAndCount(String[] words) {
+        Stemmer stemmer = new LovinsStemmer();
+        return Stream.of(words)
+                .map(stemmer::stem)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 }
