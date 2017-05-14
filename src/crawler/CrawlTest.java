49677423baf86dbb691c4dc4fd6a2a04e7ce9a4b
Bruno Jose <bjcc@cin.ufpe.br>
2017-05-14 16:30:34 -0300

    Modificando o construtor e o tipo da classe que vai representar a instancia

--- src/crawler/CrawlTest.java
+++ src/crawler/CrawlTest.java
@@ -1,34 +1,54 @@
 package crawler;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class CrawlTest {
+
 	static Thread t[] = new Thread[10];
 	static Crawl spider[] = new Crawl[10];
+	static LinkClassifier classifier;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+
+		int x;
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("(1) - Busca em Largura\n(2) - Algoritmo de Apredizagem");
+		x = in.nextInt();
+
+		if(x == 1 )crawler1();
+		else crawler2();
 		
-		spider[0] = new Crawl("http://www.sony.com/electronics/cameras", "sony");
-		spider[1] = new Crawl("http://www.nikonusa.com/en/index.page", "nikon");
-		spider[2] = new Crawl("https://www.dpreview.com/products/cameras/all", "dpreview");
-		spider[3] = new Crawl("https://shop.usa.canon.com/shop/en/catalog", "canon");
-		spider[4] = new Crawl("http://www.currys.co.uk/gbuk/cameras-and-camcorders-38-u.html", "currys");
-		spider[5] = new Crawl("http://www.wexphotographic.com/cameras/", "wexphotographic");
-		spider[6] = new Crawl("http://www.visions.ca/Catalogue/Catego"
-				+ "ry/ProductResults.aspx?categoryId=223&menu=205", "visions");
-		spider[7] = new Crawl("https://www.sigmaphoto.com/cameras", "sigmaphoto");
-		spider[8] = new Crawl("http://us.ricoh-imaging.com/index.php/shop/cameras", "ricoh");
-		spider[9] = new Crawl("https://www.newegg.com/Product/ProductList.aspx?Submit="
-				+ "ENE&IsNodeId=1&bop=And&PMSub=784%2012%2056756&PMSubCP=0cm_sp=Cat_Digita"
-				+ "l-Cameras_1-_-VisNav-_-Cameras_1", "newegg");
+	}
+
+		public static void crawler1(){
+
+			spider[0] = new Crawl("http://www.sony.com/electronics/cameras", "sony");
+			spider[1] = new Crawl("http://www.nikonusa.com/en/index.page", "nikon");
+			spider[2] = new Crawl("https://www.dpreview.com/products/cameras/all", "dpreview");
+			spider[3] = new Crawl("https://shop.usa.canon.com/shop/en/catalog", "canon");
+			spider[4] = new Crawl("http://www.currys.co.uk/gbuk/cameras-and-camcorders-38-u.html", "currys");
+			spider[5] = new Crawl("http://www.wexphotographic.com/cameras/", "wexphotographic");
+			spider[6] = new Crawl("http://www.visions.ca/Catalogue/Catego"
+					+ "ry/ProductResults.aspx?categoryId=223&menu=205", "visions");
+			spider[7] = new Crawl("https://www.sigmaphoto.com/cameras", "sigmaphoto");
+			spider[8] = new Crawl("http://us.ricoh-imaging.com/index.php/shop/cameras", "ricoh");
+			spider[9] = new Crawl("https://www.newegg.com/Product/ProductList.aspx?Submit="
+					+ "ENE&IsNodeId=1&bop=And&PMSub=784%2012%2056756&PMSubCP=0cm_sp=Cat_Digita"
+					+ "l-Cameras_1-_-VisNav-_-Cameras_1", "newegg");
 
 		/*for (int i = 0; i <= 10 ; i++) {
 			t[i] = new Thread(spider[i]);
 			t[i].start();
 		}*/
-		t[1] = new Thread(spider[1]);
-		t[1].start();
-		
-	}
+			t[1] = new Thread(spider[1]);
+			t[1].start();
+	 }
+
+	 public static void crawler2() throws Exception {
+			classifier = new LinkClassifier(0.65f);
+
+	 }
 }
--- src/crawler/LinkClassifier.java
+++ src/crawler/LinkClassifier.java
@@ -8,21 +8,34 @@ import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Stream;
 
 public class LinkClassifier {
+
     private List<Attribute> attributes;
     private Instances instances;
     private Instances trainInstances;
     private Instances testInstances;
+    static List<String> links = new ArrayList<>();
+    static List<String> positives = new ArrayList<>();
+    static List<String> next0 = new ArrayList<>();
+    static List<String> next1 = new ArrayList<>();
+    static List<String> next2 = new ArrayList<>();
 
     private Classifier classifier;
 
-    public LinkClassifier(List<String> positives, List<String> negatives, float trainRatio) throws Exception {
+    public LinkClassifier(float trainRatio) throws Exception {
 
-        List<String> links = new ArrayList<>();
-        links.addAll(negatives);
+        loadLists();
+
+        links.addAll(next0);
+        links.addAll(next1);
+        links.addAll(next2);
         links.addAll(positives);
 
         attributes = getAllAttributes(links);
@@ -30,11 +43,19 @@ public class LinkClassifier {
         instances.setClassIndex(attributes.size() - 1);
 
         for (int i = 0; i < positives.size(); i++) {
-            instances.add(createLinkInstance(positives.get(i), "p"));
+            instances.add(createLinkInstance(positives.get(i), 1));
         }
 
-        for (int i = 0; i < negatives.size(); i++) {
-            instances.add(createLinkInstance(negatives.get(i), "n"));
+        for (int i = 0; i < next0.size(); i++) {
+            instances.add(createLinkInstance(next0.get(i), 0.8));
+        }
+
+        for (int i = 0; i < next1.size(); i++) {
+            instances.add(createLinkInstance(next1.get(i), 0.6));
+        }
+
+        for (int i = 0; i < next2.size(); i++) {
+            instances.add(createLinkInstance(next2.get(i), 0.2));
         }
 
         int trainingSize = Math.round(instances.size() * trainRatio);
@@ -52,7 +73,7 @@ public class LinkClassifier {
     }
 
     public double classify(String link) throws Exception {
-        return classifier.classifyInstance(createLinkInstance(link, ""));
+        return classifier.classifyInstance(createLinkInstance(link, 0));
     }
 
     private List<Attribute> getAllAttributes(List<String> links) {
@@ -68,9 +89,14 @@ public class LinkClassifier {
             attributes.add(new Attribute(stringAttribute));
         }
         List<String> classValues = new ArrayList<>();
-        classValues.add("n");
+
         classValues.add("p");
+        classValues.add("n2");
+        classValues.add("n1");
+        classValues.add("n0");
+
         attributes.add(new Attribute("-class-", classValues));
+
         return attributes;
     }
 
@@ -80,9 +106,9 @@ public class LinkClassifier {
         int partsCount = 0;
         for (int i = 0; i < parts.length; i++) {
             partsOfParts[i] = parts[i].split("[^A-Za-z]");
-            String[] filteredParts = new String[partsOfParts[i].length - 4];
+            String[] filteredParts = new String[partsOfParts[i].length];
             for (int j = 0; j < filteredParts.length; j++) {
-                filteredParts[j] = partsOfParts[i][j + 4];
+                filteredParts[j] = partsOfParts[i][j];
             }
             partsOfParts[i] = filteredParts;
             partsCount += partsOfParts[i].length;
@@ -97,7 +123,7 @@ public class LinkClassifier {
         return parts;
     }
 
-    private Instance createLinkInstance(String link, String clazz) {
+    private Instance createLinkInstance(String link, double clazz) {
         Instance linkInstance = new DenseInstance(attributes.size());
         for (int i = 0; i < attributes.size() - 1; i++) {
             boolean attributeContained = false;
@@ -112,5 +138,37 @@ public class LinkClassifier {
         }
         linkInstance.setValue(attributes.size() - 1, clazz);
         return linkInstance;
+    }
+
+    private void loadLists() throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\bjcc\\IdeaProjects\\CamFinder\\pages\\next0.txt"));
+        while(br.ready()){
+            String linha = br.readLine();
+            next0.add(linha);
+        }
+        br.close();
+
+        BufferedReader br1 = new BufferedReader(new FileReader("C:\\Users\\bjcc\\IdeaProjects\\CamFinder\\pages\\next1.txt"));
+        while(br1.ready()){
+            String linha = br1.readLine();
+            next1.add(linha);
+        }
+        br1.close();
+
+        BufferedReader br2 = new BufferedReader(new FileReader("C:\\Users\\bjcc\\IdeaProjects\\CamFinder\\pages\\next2.txt"));
+        while(br2.ready()){
+            String linha = br2.readLine();
+            next2.add(linha);
+        }
+        br2.close();
+
+        BufferedReader br3 = new BufferedReader(new FileReader("C:\\Users\\bjcc\\IdeaProjects\\CamFinder\\pages\\positive.txt"));
+        while(br3.ready()){
+            String linha = br3.readLine();
+            positives.add(linha);
+        }
+        br3.close();
+
     }
 }
