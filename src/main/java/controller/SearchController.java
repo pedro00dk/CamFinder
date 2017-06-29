cd2cf29296537922a38303dffb540fda3770987a
Guilherme Henrique <ghps@cin.ufpe.br>
2017-06-28 21:20:54 -0300

    Merge with Rank and Index

--- src/main/java/controller/SearchController.java
+++ src/main/java/controller/SearchController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import rank.Rank;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -26,11 +27,11 @@ public class SearchController {
     static final InvertedIndex INVERTED_INDEX;
     static final Map<URL, Map<String, String>> URL_ATTRIBUTES;
     static final Map<String, List<String>> RECOMENDATIONS;
-
+    static final Rank RANK;
     static {
         InvertedIndex invertedIndex = null;
         Map<URL, Map<String, String>> urlAttributes = null;
-
+        Rank rank = null;
         try {
             Path extractedContentPath = Paths.get("C:\\Users\\Guilherme\\IdeaProjects\\CamFinder\\pages\\extracted", "extracted.data");
             ObjectInputStream is = new ObjectInputStream(Files.newInputStream(extractedContentPath));
@@ -39,12 +40,13 @@ public class SearchController {
             BlockingQueue<Pair<URL, Map<String, String>>> extractorOutput = (BlockingQueue<Pair<URL, Map<String, String>>>) is.readObject();
             invertedIndex = new InvertedIndex(extractorOutput, 5);
             urlAttributes = extractorOutput.stream().collect(Collectors.toMap(Pair::getKey, Pair::getValue));
-
+            rank = new Rank(invertedIndex);
         } catch (Exception e1) {
             e1.printStackTrace();
         } finally {
             INVERTED_INDEX = invertedIndex;
             URL_ATTRIBUTES = urlAttributes;
+            RANK = rank;
         }
 
         RECOMENDATIONS = INVERTED_INDEX != null ? INVERTED_INDEX.attributes.stream()
@@ -64,7 +66,7 @@ public class SearchController {
     public ModelAndView home() {
         ModelAndView mav = new ModelAndView("search");
         List<Pair<String, Boolean>> attributes = new ArrayList<>();
-
+        //TODO RECOMENDATIONS
 //        System.out.println(RECOMENDATIONS.toString());
         attributes.add(new Pair<>("Name", Boolean.TRUE));
         attributes.add(new Pair<>("Price", Boolean.TRUE));
@@ -84,10 +86,18 @@ public class SearchController {
                                            @RequestParam("Megapixels") String megapixel, @RequestParam("Zoom") String zoom,
                                            @RequestParam("Storage Mode") String storage_mode, @RequestParam("Sensitivity") String sensitivity,
                                            @RequestParam("Shutter Speed") String shutter_speed, @RequestParam("Sensor Size") String sensor_size) {
+        List query =  new ArrayList();
+        //TODO SHOW ATTRIBUTES
+        query.add("name."+name);
+        query.add("price."+price);
+        query.add("Shutter Speed."+shutter_speed);
+        query.add("Storage Mode."+storage_mode);
+        query.add("Sensitivity."+sensitivity);
 
+        List<URL> rank = RANK.rank(query, false);
 
-        CanonExtractor c = new CanonExtractor();
-        //Here goes the calls for Bruno's methods
+        List<Pair<URL, Map<String, String>>> collect = rank.stream().limit(10).map(url -> new Pair<>(url, URL_ATTRIBUTES.get(url))).collect(Collectors.toList());
+
         ModelAndView mav = new ModelAndView("result");
         mav.addObject("name", name);
         mav.addObject("price", price);
